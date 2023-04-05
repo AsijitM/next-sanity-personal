fc048d57d143bf823eca0501cb8b5d372911b8ea
AsijitM <asijit00@gmail.com>
2023-04-06 01:15:17 +0530

    tailwind added

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -8,8 +8,17 @@ export default async function Home() {
 
   return (
     <div>
+      <div className="max-w-5xl mx-auto p-20">
+        <h1 className="text-7xl font-extrabold ">
+          Hello I am{' '}
+          <span className="bg-gradient-to-r from-orange-400 to-purple-500  ">
+            Asijit
+          </span>
+          !
+        </h1>
+      </div>
       {projects.map((project) => (
-        <div key={project.id}>{project.name}</div>
+        <div key={project._id}>{project.name}</div>
       ))}
     </div>
   );
--- client/sanity/sanity-utils.ts
+++ client/sanity/sanity-utils.ts
@@ -1,6 +1,7 @@
+import { Project } from '@/types/Project';
 import { createClient, groq } from 'next-sanity';
 
-export async function getprojects() {
+export async function getprojects():Promise<Project[]>  {
   const client = createClient({
     projectId: '9ayzp4jf',
     dataset: 'production',
